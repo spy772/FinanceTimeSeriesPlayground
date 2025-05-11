d64d1ee18004ba1c11696d96a8d25f78f3e02997
Edward Tanurkov <77864948+spy772@users.noreply.github.com>
2025-05-11 18:57:59 -0400

    initial commit

---
+++ linear_regression/linear_regression.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+df = pd.read_csv("linear_regression/canada_per_capita_income.csv")
+
+plt.xlabel('year')
+plt.ylabel('per capita income (US$)')
+plt.scatter(df['year'], df['per capita income (US$)'], c='r', marker="+")
+plt.show()
+
+reg = linear_model.LinearRegression()
+reg.fit(df['year'], df['per capita income (US$)'])
+print(reg.coef_)
+print(reg.intercept_)
+print(reg.predict(2050))
