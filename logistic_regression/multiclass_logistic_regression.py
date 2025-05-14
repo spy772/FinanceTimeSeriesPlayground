1213aedd35cc0bb4e49a32413d5e3c76ee5b024c
Edward Tanurkov <77864948+spy772@users.noreply.github.com>
2025-05-14 08:33:31 -0400

    almost done images logistic regression

---
+++ logistic_regression/multiclass_logistic_regression.py
@@ -0,0 +1,25 @@
+from sklearn.datasets import load_digits
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
+
+digits = load_digits()
+print(digits.data[0]) # Example of an observation
+
+
+X_train, X_test, y_train, y_test = train_test_split(digits.data,digits.target, test_size=0.2)
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+print(model.score(X_test, y_test))
+print(model.predict(X_test[0:5]))
+
+# Confusion Matrix work
+y_predicted = model.predict(X_test)
+
+cm = confusion_matrix(y_test, y_predicted)
+plt.figure(figsize = (10,7))
+sn.heatmap(cm, annot=True)
+plt.xlabel('Predicted')
+plt.ylabel('Truth')
