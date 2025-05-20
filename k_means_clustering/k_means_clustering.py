037d48372a6f9ced74368864356ea222dfc0bafa
Edward Tanurkov <77864948+spy772@users.noreply.github.com>
2025-05-20 17:54:03 -0400

    completed k-means clustering

--- k_means_clustering/k_means_clustering.py
+++ k_means_clustering/k_means_clustering.py
@@ -15,4 +15,37 @@ df.drop(["sepal length (cm)", "sepal width (cm)"], axis='columns', inplace=True)
 plt.scatter(df["petal length (cm)"], df["petal width (cm)"])
 plt.xlabel("Petal Length")
 plt.ylabel("Petal Width")
-plt.show()
+# plt.show()
+
+# Cluster data
+km_model = KMeans(n_clusters=3)
+y_pred = km_model.fit_predict(df)
+
+# Place predicted values in df
+df['cluster'] = y_pred # Creates new column cluster with values of y_pred
+print(df.head())
+
+# df.cluster.unique() will return an array of unique values. We know there are
+# 3 clusters anyways so we will just display these 3 clusters
+df0 = df[df.cluster == 0]
+df1 = df[df.cluster == 1]
+df2 = df[df.cluster == 2]
+
+plt.scatter(df0['petal length (cm)'],df0['petal width (cm)'],color='blue')
+plt.scatter(df1['petal length (cm)'],df1['petal width (cm)'],color='green')
+plt.scatter(df2['petal length (cm)'],df2['petal width (cm)'],color='yellow')
+# plt.show()
+
+### Elbow plot ###
+sum_squared_errors = []
+k_range = range(1, 10)
+for k in k_range:
+    km_model = KMeans(n_clusters=k)
+    km_model.fit(df)
+    sum_squared_errors.append(km_model.inertia_) # Inertia is sse of clustering
+
+# Plot elbow plot
+plt.xlabel('K Value')
+plt.ylabel('Sum of Square Error (SSE)')
+plt.plot(k_range, sum_squared_errors)
+# plt.show()
